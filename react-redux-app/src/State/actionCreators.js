d42c0276fd032782a5ff1e8c87efb39c4c30593b
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-10 13:48:01 +0100

    created an action creator

---
+++ react-redux-app/src/State/actionCreators.js
@@ -0,0 +1,15 @@
+import * as types from "./actions";
+import axios from 'axios';
+
+
+
+export const fetchData = () => (dispatch) => {
+
+    // dispatch({
+    //     type: types.FETCH_DATA,
+    //     payload: apiData
+    // })
+}
+
+
+
